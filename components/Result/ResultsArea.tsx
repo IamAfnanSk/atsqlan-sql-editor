f6da3ce77c7f14afd769f36dcee005036c35f7eb
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2022-09-30 15:21:15 +0530

    Memoization fixes

--- components/Result/ResultsArea.tsx
+++ components/Result/ResultsArea.tsx
@@ -1,18 +1,37 @@
 import Papa from 'papaparse'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 import { AiOutlinePlayCircle } from 'react-icons/ai'
 import { BiError } from 'react-icons/bi'
+import { BsFullscreen, BsArrowCounterclockwise } from 'react-icons/bs'
+import { GrDocumentCsv } from 'react-icons/gr'
+import { TbLayoutColumns, TbLayoutRows } from 'react-icons/tb'
+import { VscJson } from 'react-icons/vsc'
 import { Column } from 'react-table'
-import { SQLQueries } from '../../global'
+import { copyToClipboard, LIMIT_FOR_ROWS, SQLQueries } from '../../global'
 import { useWebsiteContext } from '../../hooks/useWebsiteContext'
 import { errorNotification } from '../../utils/toast'
 import Table from './Table'
 
 const ResultsArea: React.FC = () => {
 	const { queryState, selectedQueryIndex, setQueryState } = useWebsiteContext()
+
 	const [columns, setColumns] = useState<Column[]>([])
 	const [data, setData] = useState<any[]>([])
+
+	const [limitResults, setLimitResults] = useState(true)
 	const [timeToResult, setTimeToResult] = useState<number>(0)
+
+	const fScreenHandle = useFullScreenHandle()
+
+	const memoizedColumns = useMemo(() => columns, [columns])
+	const memoizedData = useMemo(() => {
+		if (limitResults) {
+			return data.slice(0, LIMIT_FOR_ROWS)
+		} else {
+			return data
+		}
+	}, [limitResults, data])
 
 	useEffect(() => {
 		if (queryState === 'running') {
@@ -41,6 +60,7 @@ const ResultsArea: React.FC = () => {
 
 					setColumns(columns)
 					setData(rows)
+
 					setQueryState('success')
 
 					const endTime = Date.now()
@@ -77,7 +97,91 @@ const ResultsArea: React.FC = () => {
 				</div>
 			)}
 
-			{queryState === 'success' && <Table timeToResult={timeToResult} columns={columns} data={data} />}
+			{queryState === 'success' && (
+				<FullScreen className={`p-4 h-full w-full flex space-y-2 flex-col ${fScreenHandle.active ? 'bg-gray-100' : ''}`} handle={fScreenHandle}>
+					<Table columns={memoizedColumns} data={memoizedData} />
+
+					<div className='flex justify-between items-center'>
+						<div className='flex items-center space-x-2'>
+							<div>
+								<div
+									onClick={() => {
+										if (fScreenHandle.active) {
+											fScreenHandle.exit()
+										} else {
+											fScreenHandle.enter()
+										}
+									}}
+									className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}
+								>
+									<p className='mr-1'>{fScreenHandle.active && 'Exit'} Fullscreen</p>
+									<BsFullscreen />
+								</div>
+							</div>
+
+							{data.length > 50 && (
+								<div>
+									<div className='flex items-start'>
+										<div className='flex items-center h-5'>
+											<input
+												checked={limitResults}
+												onChange={() => setLimitResults((limitResults) => !limitResults)}
+												id='limitResults'
+												name='limitResults'
+												type='checkbox'
+												className='ring-0 cursor-pointer focus:ring-0 h-4 w-4 text-indigo-600 border-gray-300 rounded'
+											/>
+										</div>
+										<div className='ml-2 text-sm'>
+											<label htmlFor='limitResults' className='select-none cursor-pointer'>
+												Show first {LIMIT_FOR_ROWS} records only
+											</label>
+										</div>
+									</div>
+								</div>
+							)}
+						</div>
+
+						<div className='flex items-center space-x-2'>
+							<a
+								href={SQLQueries[selectedQueryIndex].dataURL}
+								target='_blank'
+								className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}
+								rel='noreferrer'
+							>
+								<p className='mr-1'>Download</p>
+								<GrDocumentCsv />
+							</a>
+
+							<div
+								onClick={() => {
+									copyToClipboard(JSON.stringify(data), 'JSON data copied to clipboard')
+								}}
+								className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}
+							>
+								<p className='mr-1'>Copy</p>
+								<VscJson />
+							</div>
+						</div>
+
+						<div className='flex items-center space-x-2'>
+							<div className='flex items-center space-x-1'>
+								<BsArrowCounterclockwise />
+								<p className='text-sm'>{timeToResult}s</p>
+							</div>
+							<div className='flex items-center space-x-1'>
+								<TbLayoutColumns />
+								<p className='text-sm'>{columns.length}</p>
+							</div>
+							<div className='flex items-center space-x-1'>
+								<TbLayoutRows />
+								<p className='text-sm'>{memoizedData.length}</p>
+								{limitResults && <p className='text-sm'> out of {data.length}</p>}
+							</div>
+						</div>
+					</div>
+				</FullScreen>
+			)}
 
 			{queryState === 'error' && (
 				<div className='h-full w-full flex flex-col items-center justify-center'>
--- components/Result/Table.tsx
+++ components/Result/Table.tsx
@@ -1,32 +1,14 @@
 /* eslint-disable react/jsx-key */
 import { Column, useSortBy, useTable } from 'react-table'
 import { FaSort, FaSortDown, FaSortUp } from 'react-icons/fa'
-import { FullScreen, useFullScreenHandle } from 'react-full-screen'
-import React, { useEffect, useMemo, useState } from 'react'
-import { copyToClipboard, LIMIT_FOR_ROWS, SQLQueries } from '../../global'
-import { BsFullscreen, BsArrowCounterclockwise } from 'react-icons/bs'
-import { GrDocumentCsv } from 'react-icons/gr'
-import { TbLayoutColumns, TbLayoutRows } from 'react-icons/tb'
-import { VscJson } from 'react-icons/vsc'
-import { useWebsiteContext } from '../../hooks/useWebsiteContext'
+import React from 'react'
 
 type TProps = {
 	columns: Column[]
 	data: any[]
-	timeToResult: number
 }
 
-const Table: React.FC<TProps> = ({ columns: columnsData, data: rowsData, timeToResult: timeTaken }) => {
-	const [dataToProcess, setDataToProcess] = useState(rowsData)
-	const [limitResults, setLimitResults] = useState(true)
-
-	const timeToResult = useMemo(() => timeTaken, [timeTaken])
-
-	const columns = useMemo(() => columnsData, [columnsData])
-	const data = useMemo(() => dataToProcess, [dataToProcess])
-
-	const { selectedQueryIndex } = useWebsiteContext()
-
+const Table: React.FC<TProps> = ({ columns, data }) => {
 	const { headerGroups, rows, prepareRow, getTableProps, getTableBodyProps } = useTable(
 		{
 			columns,
@@ -35,129 +17,44 @@ const Table: React.FC<TProps> = ({ columns: columnsData, data: rowsData, timeToR
 		useSortBy
 	)
 
-	useEffect(() => {
-		if (limitResults) {
-			setDataToProcess(rowsData.slice(0, LIMIT_FOR_ROWS))
-		} else {
-			setDataToProcess(rowsData)
-		}
-	}, [limitResults, rowsData])
-
-	const fScreenHandle = useFullScreenHandle()
-
 	return (
-		<div className='p-4 h-full w-full flex space-y-2 flex-col'>
-			<div className='overflow-scroll relative rounded-lg'>
-				<FullScreen className={`${fScreenHandle.active ? 'overflow-scroll' : ''}`} handle={fScreenHandle}>
-					<table {...getTableProps()} className='divide-y divide-gray-300 text-left'>
-						<thead className='bg-white'>
-							{headerGroups.map((headerGroup) => (
-								<tr {...headerGroup.getHeaderGroupProps()} className='divide-x divide-gray-200'>
-									{headerGroup.headers.map((column) => (
-										<th
-											{...column.getHeaderProps((column as any).getSortByToggleProps())}
-											scope='col'
-											className='sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-4 py-1.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter table-cell'
-										>
-											<p className='flex items-center space-x-2'>
-												<span>{column.render('Header')}</span>
-												<span>{(column as any).isSorted ? (column as any).isSortedDesc ? <FaSortDown /> : <FaSortUp /> : <FaSort />}</span>
-											</p>
-										</th>
-									))}
-								</tr>
+		<div className='overflow-scroll relative rounded-lg'>
+			<table {...getTableProps()} className='divide-y divide-gray-300 text-left'>
+				<thead className='bg-white'>
+					{headerGroups.map((headerGroup) => (
+						<tr {...headerGroup.getHeaderGroupProps()} className='divide-x divide-gray-200'>
+							{headerGroup.headers.map((column) => (
+								<th
+									{...column.getHeaderProps((column as any).getSortByToggleProps())}
+									scope='col'
+									className='sticky top-0 z-10 border-b border-gray-300 bg-gray-50 bg-opacity-75 px-4 py-1.5 text-left text-sm font-semibold text-gray-900 backdrop-blur backdrop-filter table-cell'
+								>
+									<p className='flex items-center space-x-2'>
+										<span>{column.render('Header')}</span>
+										<span>{(column as any).isSorted ? (column as any).isSortedDesc ? <FaSortDown /> : <FaSortUp /> : <FaSort />}</span>
+									</p>
+								</th>
 							))}
-						</thead>
-						<tbody {...getTableBodyProps()} className='divide-y divide-gray-200 bg-white'>
-							{rows.map((row) => {
-								prepareRow(row)
-								return (
-									<tr {...row.getRowProps()} className='divide-x divide-gray-200'>
-										{row.cells.map((cell) => {
-											return (
-												<td className='whitespace-nowrap px-4 py-1.5 text-sm text-gray-500' {...cell.getCellProps()}>
-													{cell.render('Cell')}
-												</td>
-											)
-										})}
-									</tr>
-								)
-							})}
-						</tbody>
-					</table>
-				</FullScreen>
-			</div>
-
-			<div className='flex justify-between items-center'>
-				<div className='flex items-center space-x-2'>
-					<div>
-						<div onClick={() => fScreenHandle.enter()} className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}>
-							<p className='mr-1'>Fullscreen</p>
-							<BsFullscreen />
-						</div>
-					</div>
-
-					{rowsData.length > 50 && (
-						<div>
-							<div className='flex items-start'>
-								<div className='flex items-center h-5'>
-									<input
-										checked={limitResults}
-										onChange={() => setLimitResults((limitResults) => !limitResults)}
-										id='limitResults'
-										name='limitResults'
-										type='checkbox'
-										className='ring-0 cursor-pointer focus:ring-0 h-4 w-4 text-indigo-600 border-gray-300 rounded'
-									/>
-								</div>
-								<div className='ml-2 text-sm'>
-									<label htmlFor='limitResults' className='select-none cursor-pointer'>
-										Show first {LIMIT_FOR_ROWS} records only
-									</label>
-								</div>
-							</div>
-						</div>
-					)}
-				</div>
-
-				<div className='flex items-center space-x-2'>
-					<a
-						href={SQLQueries[selectedQueryIndex].dataURL}
-						target='_blank'
-						className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}
-						rel='noreferrer'
-					>
-						<p className='mr-1'>Download</p>
-						<GrDocumentCsv />
-					</a>
-
-					<div
-						onClick={() => {
-							copyToClipboard(JSON.stringify(data), 'JSON data copied to clipboard')
-						}}
-						className={`cursor-pointer text-xs py-1 px-2 flex justify-center items-center bg-white shadow-sm rounded-md`}
-					>
-						<p className='mr-1'>Copy</p>
-						<VscJson />
-					</div>
-				</div>
-
-				<div className='flex items-center space-x-2'>
-					<div className='flex items-center space-x-1'>
-						<BsArrowCounterclockwise />
-						<p className='text-sm'>{timeToResult}s</p>
-					</div>
-					<div className='flex items-center space-x-1'>
-						<TbLayoutColumns />
-						<p className='text-sm'>{columns.length}</p>
-					</div>
-					<div className='flex items-center space-x-1'>
-						<TbLayoutRows />
-						<p className='text-sm'>{rows.length}</p>
-						{limitResults && <p className='text-sm'> out of {rowsData.length}</p>}
-					</div>
-				</div>
-			</div>
+						</tr>
+					))}
+				</thead>
+				<tbody {...getTableBodyProps()} className='divide-y divide-gray-200 bg-white'>
+					{rows.map((row) => {
+						prepareRow(row)
+						return (
+							<tr {...row.getRowProps()} className='divide-x divide-gray-200'>
+								{row.cells.map((cell) => {
+									return (
+										<td className='whitespace-nowrap px-4 py-1.5 text-sm text-gray-500' {...cell.getCellProps()}>
+											{cell.render('Cell')}
+										</td>
+									)
+								})}
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
 		</div>
 	)
 }
